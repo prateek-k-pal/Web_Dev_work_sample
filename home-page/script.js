69208f2d22e00209b10e1911043d2c92a078b618
prateek-k-pal <74899303+prateek-k-pal@users.noreply.github.com>
2021-06-15 10:23:53 +0530

    Create script.js

---
+++ home-page/script.js
@@ -0,0 +1,39 @@
+function dark(){
+  (document.getElementById("body")).className = "dark";
+}
+
+function light(){
+  (document.getElementById("body")).className = "light";
+}
+
+function roseanna(){
+  (document.getElementById("body")).className = "roseanna";
+}
+
+function sexyblue(){
+  (document.getElementById("body")).className = "sexyblue";
+}
+
+function purplelove(){
+  (document.getElementById("body")).className = "purplelove";
+}
+
+function shadesofgrey(){
+  (document.getElementById("body")).className = "shadesofgrey";
+}
+
+function lostmemory(){
+  (document.getElementById("body")).className = "lostmemory";
+}
+
+function lush(){
+  (document.getElementById("body")).className = "lush";
+}
+
+function greenbeach(){
+  (document.getElementById("body")).className = "greenbeach";
+}
+
+function frost(){
+  (document.getElementById("body")).className = "frost";
+}
